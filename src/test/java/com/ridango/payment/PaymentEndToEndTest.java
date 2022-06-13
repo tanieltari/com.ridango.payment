05f9139f31579cd481836d264288d591ef026511
Taniel Tari <tari.taniel@gmail.com>
2022-06-13 22:27:53 +0300

    Initial commit

---
+++ src/test/java/com/ridango/payment/PaymentEndToEndTest.java
@@ -0,0 +1,9 @@
+package com.ridango.payment;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PaymentApplication.class)
+@AutoConfigureMockMvc
+public class PaymentEndToEndTest {
+}
